4f2e0929a5dbff5ec8a381457a06fd0dad2f91c1
jcalca1 <jeffcalcagno@gmail.com>
2015-07-25 00:34:03 -0500

    rcav

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,30 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+def square_root
+
+  @num = params["num1"]
+@square = Math.sqrt(@num.to_f)
+render("square_root.html.erb")
+end
+
+def random
+
+  @num = params["num1"]
+  @num2 = params["num2"]
+  @rand = rand(51)+50
+render("random.html.erb")
+end
+
+def payment
+
+  @rate = number_to_percentage(params["num1"])
+  @years = params["num2"].to_f
+  @loan_amount = params["num3"].to_f
+@payment=(@rate*@loan_amount)/(1-(1+@rate)**(-@years))
+@test= number_to_currency(@rate)
+render("payment.html.erb")
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  get("/instructions", { :controller => "calculations", :action => "instructions" })
+ get("/square_root/:num1", { :controller => "calculations", :action => "square_root" })
+get("/random/:num1/:num2", { :controller => "calculations", :action => "random" })
+get("/payment/:num1/:num2/:num3", { :controller => "calculations", :action => "payment" })
 end
